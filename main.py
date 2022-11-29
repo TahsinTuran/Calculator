70071bb0ee3227b8cf224ecae487a9fb9f314653
Tahsin Turan <86520268+TahsinTuran@users.noreply.github.com>
2022-11-29 21:52:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from replit import clear
+def clear() -> None:
+    """Clear the terminal."""
+    print("\033[H\033[2J", end="", flush=True)
 logo = """
  _____________________
 |  _________________  |
